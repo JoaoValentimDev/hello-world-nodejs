95b4ee493d6ec1f5ac621b3ca4fdfb5d9b238c51
JoaoTheodoroDev <theodorojoaogabrielvalentim@gmail.com>
2020-09-10 23:19:53 -0300

    Hello World em Node.js

--- hello.js
+++ hello.js
@@ -3,11 +3,11 @@ const http = require('http');
         
 // criando meu servidor
 const meuServidor = http.createServer(function (requisicao, resposta) {
-	resposta.setHeader('Content-type', 'text/plain');
-	resposta.end('Hello World');
+  resposta.setHeader('Content-type', 'text/plain');
+  resposta.end('Hello World');
 });
         
 // Dando uma porta para o meu servidor
 meuServidor.listen(3000, function() {
-	console.log('Servidor rodando na porta 3000...');
+  console.log('Servidor rodando na porta 3000...');
 });
